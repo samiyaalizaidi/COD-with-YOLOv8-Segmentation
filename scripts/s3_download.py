322b0cf19f978d59344b8b3d6798e7d163aac67a
Samiya Ali Zaidi <samiyaalizaidi@gmail.com>
2024-01-02 11:51:33 +0500

    script to download the zip files from the s3 bucket

---
+++ scripts/s3_download.py
@@ -0,0 +1,19 @@
+import boto3
+
+BUCKET_NAME = "cod-ash"
+
+# enter authentication credentials
+s3 = boto3.resource('s3', aws_access_key_id = "Hussain-IAM-access",
+                          aws_secret_access_key= "7RzpVD9}")
+
+KEY = "cod-ash/YOLOv8-Combined-Split.zip" # replace with your object key
+
+try:
+  s3.Bucket(BUCKET_NAME).download_file(KEY, "/Downloads/YOLOv8-Combined-Split.zip")
+
+except botocore.exceptions.ClientError as e:
+  if e.response['Error']['Code'] == "404":
+    print("The object does not exist.")
+  else:
+    raise
+
