5b58a7ed87aefd2fe3e6f47f24fb045e2747aac2
Samiya Ali Zaidi <95542517+samiyaalizaidi@users.noreply.github.com>
2024-01-04 20:43:18 +0500

    Update path

--- scripts/s3_download.py
+++ scripts/s3_download.py
@@ -7,10 +7,10 @@ BUCKET_NAME = "cod-ash"
 s3 = boto3.resource('s3', aws_access_key_id = "",
                           aws_secret_access_key= "")
 
-KEY = "YOLOv8-Combined-Split.zip" # replace with your object key
+KEY = "weights/best_100epochs_v8x.pt" # replace with your object key
 
 try:
-  s3.Bucket(BUCKET_NAME).download_file(KEY, "/Downloads/YOLOv8-Combined-Split.zip")
+  s3.Bucket(BUCKET_NAME).download_file(KEY, "/Downloads/best_100epochs_v8x.pt")
 
 except botocore.exceptions.ClientError as e:
   if e.response['Error']['Code'] == "404":
