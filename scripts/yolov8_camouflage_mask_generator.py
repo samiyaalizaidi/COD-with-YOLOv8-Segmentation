0f8d2fde1c4521a0affdb638a108230d189afdf3
Samiya Ali Zaidi <samiyaalizaidi@gmail.com>
2024-01-06 23:49:45 +0500

    fixed the code

--- scripts/yolov8_camouflage_mask_generator.py
+++ scripts/yolov8_camouflage_mask_generator.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import torch
+import numpy as np
 from tqdm import tqdm
 from pathlib import Path
 from ultralytics import YOLO
@@ -11,7 +12,7 @@ def process_images(input_dir, output_dir, model_path):
 
     # create output directory for predictions to be saved
     Path(output_dir).mkdir(parents=True, exist_ok=True)
-    counter =0
+    counter = 0
 
 
     for img_name in tqdm(os.listdir(input_dir)):
@@ -40,18 +41,25 @@ def process_images(input_dir, output_dir, model_path):
                   obj_mask = torch.any(obj_masks, dim=0).int() * 255
 
                   # Saving the binary mask output of the prediction
-                  cv2.imwrite(os.path.join(output_dir, f'{orig_img_name}_binary_mask.jpg'), obj_mask.cpu().numpy())
+                  cv2.imwrite(os.path.join(output_dir, f'{orig_img_name}.png'), obj_mask.cpu().numpy())
+
             except Exception as e:
-              counter+=1
+              counter += 1
+              # creating an image with the same dimensions to have an empty binary mask with the same name
+              shape = test_img.shape
+              empty_img = np.zeros(shape, dtype = np.uint8)
+              cv2.imwrite(os.path.join(output_dir, f'{orig_img_name}.png'), empty_img)    
+              # display on terminal when theres no object found.   
               print(f"error: {e}")
               print("\n"*3)
               print(f'Image Name {orig_img_name}')
               print("\n"*3)
-    print(f'Corrupted Images {counter}')
+
+    print(f'Images with no object detected: {counter}')
               
 # usage | following values need to adjusted at the time of execution based on relevant experiment requirement
-INPUT_DIR = "/content/CAMO/test/images/"
-OUTPUT_DIR = "./test_output1/"
-MODEL_PATH = "/content/best.pt"
+INPUT_DIR = "D:/YOLOv8-Combined-Split/YOLOv8-Combined-Split/test/images"
+OUTPUT_DIR = "D:/YOLOv8-Combined-Split/predictions"
+MODEL_PATH = "models/best_8m_100epochs.pt"
 
 process_images(input_dir=INPUT_DIR, output_dir=OUTPUT_DIR, model_path=MODEL_PATH)
