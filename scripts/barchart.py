3afeab0df440d8cce5ea3a677cdcf3ed6dc3d94f
Samiya Ali Zaidi <samiyaalizaidi@gmail.com>
2024-03-06 21:14:30 +0500

    included arg for width

--- scripts/barchart.py
+++ scripts/barchart.py
@@ -4,7 +4,7 @@ import numpy as np
 # Define multiple datasets (you can modify this according to your datasets)
 datasets = {
     'COD-10K': {'total_images': 6000, 'images_used': 4000},
-    'MoCA': {'total_images': 22139, 'images_used': 4000},
+    'MoCA': {'total_images': 18321, 'images_used': 4000},
     'NC4K': {'total_images': 4121, 'images_used': 4000}
 }
 
@@ -12,20 +12,20 @@ datasets = {
 categories = ['Images Used', 'Images Unused']
 dataset_names = list(datasets.keys())
 num_datasets = len(dataset_names)
-bar_width = 0.25  # Width of the bars
+bar_width = 0.5  # Width of the bars
 
 # Calculate the number of unused images and used images for each dataset
 unused_images = [datasets[name]['total_images'] - datasets[name]['images_used'] for name in dataset_names]
 used_images = [datasets[name]['images_used'] for name in dataset_names]
 
 # Create the bar chart
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(8, 8))
 
 # Plot bars for unused images
-bars1 = plt.bar(dataset_names, used_images, color='lightblue', label='Images Being Used')
+bars1 = plt.bar(dataset_names, used_images, color='lightblue', label='Images Being Used', width=bar_width)
 
 # Plot bars for used images on top of the unused images
-bars2 = plt.bar(dataset_names, unused_images, bottom=used_images, color='purple', label='Images Not Being Used')
+bars2 = plt.bar(dataset_names, unused_images, bottom=used_images, color='purple', label='Images Not Being Used', width=bar_width)
 
 # Adding labels and title
 plt.xlabel('Datasets')
